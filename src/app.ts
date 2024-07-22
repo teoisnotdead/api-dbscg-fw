0bfd15a97afcd052dea4af1fb0a108be0a99995f
TEO <alfredo.ssm@gmail.com>
2024-07-22 10:52:43 -0400

    Se corrige componente de rutas para decks

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import './infrastructure/database/mongoose'
 import { cardRoutes } from './routes/card.routes'
+import { deckRoutes } from './routes/deck.routes'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,7 +12,7 @@ app.get('/', (req, res) => {
   res.send('<h1>DBSCG FW Server!!!</h1>')
 })
 app.use('/api/cards', cardRoutes)
-app.use('/api/decks', cardRoutes)
+app.use('/api/decks', deckRoutes)
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
