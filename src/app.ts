69e106c1fef57c4992f22845aef959ff2db3d9e2
TEO <alfredo.ssm@gmail.com>
2024-07-27 22:52:35 -0400

    Se modulariza un poco mas las rutas para limpieza de app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,19 +2,22 @@ import express, { Request, Response, NextFunction } from 'express'
 import './domain/interfaces/auth.interface'
 import './infrastructure/database/mongoose'
 import bodyParser from 'body-parser'
-
-import { cardRoutes,
-  deckRoutes,
-  userRoutes,
-  authRoutes,
-  adminRoutes
-} from './routes'
+import cors from 'cors'
+import { registerRoutes } from './routes'
 import morganMiddleware from './middlewares/morgan.middleware'
 import logger from './utils/logger'
 import { config } from './config'
 
 const app = express()
 const port = config.PORT
+
+// Configurar CORS
+const corsOptions: cors.CorsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 
 // Middleware para la poblacion de datos
 app.use(bodyParser.json({ limit: '50mb' })) // Aumentar el límite de tamaño de carga útil
@@ -24,14 +27,13 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })) // Aumentar el
 app.use(morganMiddleware)
 
 app.use(express.json())
+
+// Registrar rutas
+registerRoutes(app)
+
 app.get('/', (req, res) => {
   res.send('<h1>DBSCG FW Server!!!</h1>')
 })
-app.use('/api/cards', cardRoutes)
-app.use('/api/decks', deckRoutes)
-app.use('/api/user', userRoutes)
-app.use('/api/auth', authRoutes)
-app.use('/api/admin', adminRoutes)
 
 // Middleware para manejo de errores
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,14 @@
-export { cardRoutes } from './card.routes'
-export { deckRoutes } from './deck.routes'
-export { userRoutes } from './user.routes'
-export { authRoutes } from './auth.routes'
-export { adminRoutes } from './admin.routes'
+import { Application } from 'express'
+import { cardRoutes } from './card.routes'
+import { deckRoutes } from './deck.routes'
+import { userRoutes } from './user.routes'
+import { authRoutes } from './auth.routes'
+import { adminRoutes } from './admin.routes'
+
+export const registerRoutes = (app: Application): void => {
+  app.use('/api/cards', cardRoutes)
+  app.use('/api/decks', deckRoutes)
+  app.use('/api/user', userRoutes)
+  app.use('/api/auth', authRoutes)
+  app.use('/api/admin', adminRoutes)
+}
